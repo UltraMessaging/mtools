8c338fec86c20af04c2b9a65f9bb5c946759c3fa
Steve Ford <sford@geeky-boy.com>
2024-05-09 10:39:58 -0400

    updates

--- cleanup.sh
+++ cleanup.sh
@@ -5,3 +5,6 @@ rm -rf x x.* *.x *.tmp tmp.* *.tmp[0-9] ~* .DS_Store .vs *.log *.recipe *.sdf *.
 rm -rf x64 */x64 Debug */Debug
 
 rm -rf */*.vcxproj.user
+
+# Fixes VS error "System.Collections.IStructuralEquatable is not a GenericTypeDefinition. MakeGenericType may only be called on a type for which Type.IsGenericTypeDefinition is true."
+rm -rf ~/sford/AppData/Local/Microsoft/VisualStudio/11.0/ComponentModelCache
