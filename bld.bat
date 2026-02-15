cac65ae40978f9d10cdb53dc753572f5cfe9a4ed
Steve Ford <sford@geeky-boy.com>
2026-02-15 09:40:45 -0500

    updates

--- bld.bat
+++ bld.bat
@@ -1,7 +1,7 @@
 rem bld.bat
 
-cl /std:c11 /W4 /O2 /MT /nologo msend.c /Fe:Win64\msend.exe
+cl /std:c11 /W4 /O2 /MT /nologo msend.c ws2_32.lib /Fe:Win64\msend.exe
 
-cl /std:c11 /W4 /O2 /MT /nologo mdump.c /Fe:Win64\mdump.exe
+cl /std:c11 /W4 /O2 /MT /nologo mdump.c ws2_32.lib /Fe:Win64\mdump.exe
 
-cl /std:c11 /W4 /O2 /MT /nologo mpong.c /Fe:Win64\mpong.exe
+cl /std:c11 /W4 /O2 /MT /nologo mpong.c ws2_32.lib /Fe:Win64\mpong.exe
