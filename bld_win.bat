901130d27c09588c0ed2201051ca8293593e5452
Steve Ford <sford@geeky-boy.com>
2026-02-16 12:57:49 -0500

    updates

--- bld_win.bat
+++ bld_win.bat
@@ -1,7 +1,7 @@
 rem bld.bat
 
-cl /std:c11 /W4 /O2 /MT /nologo msend.c ws2_32.lib /Fe:Win64\msend.exe
+cl /std:c11 /W4 /O2 /MT /nologo /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE msend.c ws2_32.lib /Fe:Win64\msend.exe
 
-cl /std:c11 /W4 /O2 /MT /nologo mdump.c ws2_32.lib /Fe:Win64\mdump.exe
+cl /std:c11 /W4 /O2 /MT /nologo /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE mdump.c ws2_32.lib /Fe:Win64\mdump.exe
 
-cl /std:c11 /W4 /O2 /MT /nologo mpong.c ws2_32.lib /Fe:Win64\mpong.exe
+cl /std:c11 /W4 /O2 /MT /nologo /D_CRT_SECURE_NO_WARNINGS /D_CRT_NONSTDC_NO_DEPRECATE mpong.c ws2_32.lib /Fe:Win64\mpong.exe
